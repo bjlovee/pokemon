bc3afbf6fff2d368e2210d8f6828e62f3300e500
bjlovee <bj000051@yahoo.com>
2022-10-17 20:11:00 -0400

    completed pokemon

--- views/pokemons/New.jsx
+++ views/pokemons/New.jsx
@@ -11,6 +11,7 @@ class New extends React.Component {
             <form method='POST' action="/pokemon">
                 Name: <input type="text" name="name" placeholder='Name of Pokemon Here'></input><br/>
                Type: <input type="text" name="type" placeholder='Type of Pokemon Here'></input><br/>
+               Image: <input type="text" name="image" placeholder='insert img url'></input><br/>
                 Is Ready To Fight: <input type="checkbox" name="readyToFight"></input><br/>
                 <input type="submit" value="Submit Pokemon"></input>
             </form>
--- views/pokemons/Show.jsx
+++ views/pokemons/Show.jsx
@@ -4,16 +4,12 @@ const Default = require('../layouts/Default.jsx')
 class Show extends React.Component {
    
     render(){
-        const {name, type, readyToFight, _id} = this.props.pokemon
+        const {name, type, readyToFight, _id, image} = this.props.pokemon
         const capName = name[0].toUpperCase() + name.substring(1)
         return(
         <Default title={`${capName} Show Page`} pokemon={this.props.pokemon}>
             <p>{capName} type is {type} and {readyToFight? 'it\'s ready to fight': 'it\'s not ready to fight'}</p>
-            <a href="https://img.pokemondb.net/artwork/large/geodude.jpg">Geodude Img</a><br/>
-            <a href="https://img.pokemondb.net/artwork/avif/slowbro.avif">Slowbro</a><br/>
-            <a href="https://img.pokemondb.net/artwork/avif/gastly.avif">Gastly</a><br/>
-            <a href="https://img.pokemondb.net/artwork/avif/voltorb.avif">Voltorb</a><br/>
-            <a href="https://img.pokemondb.net/artwork/avif/snorlax.avif">Snorlax</a>
+            {image ? <img src={image}/> :''}
         </Default>
         )
    } 
